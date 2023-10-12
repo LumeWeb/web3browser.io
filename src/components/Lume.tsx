45632397f0250a8a849fd750717308f7bee1365c
Derrick Hammer <derrick@derrickhammer.com>
2023-10-12 13:56:18 -0400

    fix: revert and only render LumeIdentity when not logged in, and disable it when not inited

--- src/components/Lume.tsx
+++ src/components/Lume.tsx
@@ -8,22 +8,22 @@ import {
 export default function () {
   const { isLoggedIn, ready, inited } = useLume();
 
-  let loginDisabled = !inited || isLoggedIn;
-
   return (
     <>
-      <LumeIdentity>
-        <LumeIdentityTrigger asChild disabled={loginDisabled}>
-          {
-            <button
-              className="ml-2 w-full rounded-full bg-[hsl(113,49%,55%)] text-black disabled:pointer-events-none disabled:opacity-50"
-              disabled={loginDisabled}
-            >
-              Login
-            </button>
-          }
-        </LumeIdentityTrigger>
-      </LumeIdentity>
+      {!isLoggedIn && (
+        <LumeIdentity>
+          <LumeIdentityTrigger asChild disabled={!inited}>
+            {
+              <button
+                className="ml-2 w-full rounded-full bg-[hsl(113,49%,55%)] text-black disabled:pointer-events-none disabled:opacity-50"
+                disabled={!inited}
+              >
+                Login
+              </button>
+            }
+          </LumeIdentityTrigger>
+        </LumeIdentity>
+      )}
       {isLoggedIn && ready && <LumeDashboard />}
     </>
   );
