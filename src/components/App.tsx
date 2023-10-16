3babca37875a951927da188e3c63f72fa17bf1f0
Derrick Hammer <derrick@derrickhammer.com>
2023-10-16 12:54:21 -0400

    Revert "fix: wrap child components in a memo"
    This reverts commit 14c725ccd953ad00810a4e8f585e4fad337b1f7d.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,20 +5,15 @@ import {
 } from "@/components/Browser.tsx";
 import Lume from "@/components/Lume.tsx";
 import { LumeProvider } from "@lumeweb/sdk";
-import { memo } from "react";
 
 export default function () {
-  const MemeoedNavigator = memo(Navigator);
-  const MemeoedLume = memo(Lume);
-  const MemeoedBrowser = memo(Browser);
-
   return (
     <LumeProvider>
       <BrowserStateProvider>
         <>
           <header className="relative h-14 px-2 pl-2 py-2 w-full bg-neutral-900 flex">
             <div className="relative h-full w-full rounded-full bg-neutral-800 border border-neutral-700 flex items-center [>input:focus]:ring-2 [>input:focus]:ring-white">
-              <MemeoedNavigator />
+              <Navigator />
             </div>
             <div className="w-32 flex justify-end">
               <Lume />
